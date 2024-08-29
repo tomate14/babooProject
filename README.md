bec1a5c65bf38a026c6ed89ec42b43aa77545f0e
Maximiliano Roselli <maxiroselli@gmail.com>
2024-08-29 14:41:42 -0300

    Agregar perfiles de configuracion y script de creacion de tablas para la base de test

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # babooProject
+
+Para cambiar de perfil.
+* Actualizar el Dockerfile por el perfil necesario, ej, test
+* Eliminar el -test o -prod del docker compose a utilizar.
+* Ejecutar el build del backend de la config necesaria con el perfil
--- script/creacion_tablas.sql
+++ script/creacion_tablas.sql
@@ -8,26 +8,69 @@ CREATE TABLE `CAJA` (
   `gastos` int NOT NULL,
   `ganancia` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `CLIENTE` (
   `dni` int NOT NULL,
   `nombre` text NOT NULL,
   `fecha_nacimiento` text,
-  `direccion` text,
-  `telefono` text,
+  `direccion` text NOT NULL,
+  `telefono` text NOT NULL,
   `email` text,
-  `cuit` text NOT NULL,
+  `cuit` text,
   `fecha_alta` text,
+  `tipo_usuario` int NOT NULL,
+  `porcentaje_remarcar` int DEFAULT NULL,
   PRIMARY KEY (`dni`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
+
+CREATE TABLE `PEDIDO` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dni_cliente` int NOT NULL,
+  `fecha_pedido` text NOT NULL,
+  `total` int NOT NULL,
+  `estado` varchar(50) NOT NULL,
+  `tipo_pedido` int NOT NULL,
+  `estado_envio` int NOT NULL,
+  `numero_comprobante` varchar(8) NOT NULL,
+  `con_sena` tinyint NOT NULL,
+  `descripcion` text NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `PRODUCTO` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` text NOT NULL,
+  `codigo_barra` text,
+  `precio_compra` int NOT NULL,
+  `precio_venta` int NOT NULL,
+  `id_proveedor` int NOT NULL,
+  `stock` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_proveedor_idx` (`id_proveedor`),
+  CONSTRAINT `id_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `CLIENTE` (`dni`)
+);
+
+CREATE TABLE `COMPROBANTE_ITEM` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_pedido` int NOT NULL,
+  `id_producto` int NOT NULL,
+  `stock` int NOT NULL,
+  `precio` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_pedido_idx` (`id_pedido`),
+  KEY `id_producto_idx` (`id_producto`),
+  CONSTRAINT `id_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `PEDIDO` (`id`),
+  CONSTRAINT `id_producto` FOREIGN KEY (`id_producto`) REFERENCES `PRODUCTO` (`id`)
+);
 
 CREATE TABLE `NUMERACION` (
   `id` int NOT NULL AUTO_INCREMENT,
   `numero_comprobante` int NOT NULL,
   `tipo_comprobante` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `PAGO` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -37,18 +80,11 @@ CREATE TABLE `PAGO` (
   `forma_pago` int NOT NULL,
   `descripcion` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
-CREATE TABLE `PEDIDO` (
+
+CREATE TABLE `TIPO_USUARIO` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `dni_cliente` int NOT NULL,
-  `fecha_pedido` text NOT NULL,
-  `total` int NOT NULL,
-  `estado` varchar(50) NOT NULL,
-  `descripcion` text NOT NULL,
-  `tipo_pedido` int NOT NULL,
-  `estado_envio` int NOT NULL,
-  `numero_comprobante` varchar(8) NOT NULL,
-  `con_sena` tinyint NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
